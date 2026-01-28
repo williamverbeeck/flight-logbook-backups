623022974cf65731df2f4b742ffc2565936cafe8
William Verbeeck <williamverbeeck@Air-van-William.lan>
2026-01-28 21:03:03 +0100

    database updated.

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base
+import os
+
+os.makedirs("data", exist_ok=True)
 
 DATABASE_URL = "sqlite:///data/logbook.db"
 
@@ -9,3 +13,4 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
